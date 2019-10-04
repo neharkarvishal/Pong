a1b9c80633ffe6cdd5ebd486d79df1c053896f7f
Vishal <neharkar.vishal@gmail.com>
2019-10-05 02:40:27 +0530

    Step 5

--- My_First_Cpp_Game/game.cpp
+++ My_First_Cpp_Game/game.cpp
@@ -5,7 +5,9 @@
 float player_1_p, player_1_dp, player_2_p, player_2_dp;
 float arena_half_size_x = 85, arena_half_size_y = 45;
 float player_half_size_x = 2.5, player_half_size_y = 12;
-float ball_p_x, ball_p_y, ball_dp_x = 100, ball_dp_y, ball_half_size = 1;
+float ball_p_x, ball_p_y, ball_dp_x = 130, ball_dp_y, ball_half_size = 1;
+
+int player_1_score, player_2_score;
 
 internal void
 simulate_player(float *p, float *dp, float ddp, float dt) {
@@ -39,8 +41,16 @@ simulate_game(Input* input, float dt) {
 	draw_rect(0, 0, arena_half_size_x, arena_half_size_y, 0xffaa33);
 
 	float player_1_ddp = 0.f;
+#if 0
 	if (is_down(BUTTON_UP)) player_1_ddp += 2000;
 	if (is_down(BUTTON_DOWN)) player_1_ddp -= 2000;
+#else
+	//if (ball_p_y > player_1_p+2.f) player_1_ddp += 1300;
+	//if (ball_p_y < player_1_p-2.f) player_1_ddp -= 1300;
+	player_1_ddp = (ball_p_y - player_1_p)*100;
+	if (player_1_ddp > 1300) player_1_ddp = 1300;
+	if (player_1_ddp < -1300) player_1_ddp = -1300;
+#endif
 
 	float player_2_ddp = 0.f;
 	if (is_down(BUTTON_W)) player_2_ddp += 2000;
@@ -80,14 +90,19 @@ simulate_game(Input* input, float dt) {
 			ball_dp_y = 0;
 			ball_p_x = 0;
 			ball_p_y = 0;
+			player_1_score++;
 		}
 		else if (ball_p_x - ball_half_size < -arena_half_size_x) {
 			ball_dp_x *= -1;
 			ball_dp_y = 0;
 			ball_p_x = 0;
 			ball_p_y = 0;
+			player_2_score++;
 		}
 	}
+
+	draw_number(player_1_score, -10, 40, 1.f, 0xbbffbb);
+	draw_number(player_2_score, 10, 40, 1.f, 0xbbffbb);
 
 	// Rendering
 	draw_rect(ball_p_x, ball_p_y, ball_half_size, ball_half_size, 0xffffff);
